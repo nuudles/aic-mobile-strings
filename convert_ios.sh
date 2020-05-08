2a035542fc535793fb2c5b23a9283b446b0b38e9
Christopher Luu <nuudles@gmail.com>
2020-05-08 16:44:25 -0400

    Add iOS twine to strings conversion script

---
+++ convert_ios.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if (( $# != 1 ))
+then
+	echo "Usage: ./convert_ios.sh [path_to_ios_localization_folder]"
+	exit 1
+fi
+
+twineFiles=("access_card.twine" "audio.twine" "base.twine" "info.twine" "localization.twine" "localization_ui.twine" "location_ui.twine" "map.twine" "media_ui.twine" "search.twine")
+stringsFiles=("AccessCard.strings" "Audio.strings" "Base.strings" "Info.strings" "Localization.strings" "LocalizationUI.strings" "LocationUI.strings" "Map.strings" "MediaUI.strings" "Search.strings")
+
+for ((i=0;i<${#twineFiles[@]};++i)); do
+	twineFile=${twineFiles[i]}
+	stringsFile=${stringsFiles[i]}
+
+	twine generate-localization-file --lang en $twineFile $1/en.lproj/$stringsFile
+	twine generate-localization-file --lang es $twineFile $1/es.lproj/$stringsFile
+	twine generate-localization-file --lang zh $twineFile $1/zh-Hans.lproj/$stringsFile
+
+	echo $twineFile converted
+done
