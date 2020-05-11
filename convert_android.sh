010f41eee7ea675c8f0a1b09a593282039b11e78
Christopher Luu <nuudles@gmail.com>
2020-05-11 10:45:46 -0400

    Add Android twine conversion script

---
+++ convert_android.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if (( $# != 1 ))
+then
+	echo "Usage: ./convert_ios.sh [path_to_android_folder]"
+	exit 1
+fi
+
+twineFiles=("access_card.twine" "audio.twine" "base.twine" "info.twine" "localization.twine" "localization_ui.twine" "location_ui.twine" "map.twine" "media_ui.twine" "search.twine")
+
+for twineFile in "${twineFiles[@]}"; do
+	name=$(basename $twineFile .twine)
+
+	twine generate-localization-file --format android --lang en $twineFile $1/$name/src/main/res/values-en/strings.xml
+	twine generate-localization-file --format android --lang es $twineFile $1/$name/src/main/res/values-es/strings.xml
+	twine generate-localization-file --format android --lang zh $twineFile $1/$name/src/main/res/values-zh/strings.xml
+
+	echo $twineFile converted
+done
